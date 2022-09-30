1f34ac1f92d0a4aac18f04d6ac4f91ae7b6e4d33
Ke2777 <kir@ya.ru>
2022-09-30 22:20:09 +0300

    First commit

---
+++ Helper.cs
@@ -0,0 +1,15 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Controls;
+
+//namespace DemoExamWPF
+//{
+//    public static class Helper
+//    {
+//        public EnvironmentVariableTarget MyProperty { get; set; }
+//        System.Windows.Forms.MessageBox.Show("Test");    
+//    }
+//}
